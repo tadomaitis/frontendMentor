a373577d1e9d9ef588418278360848185a2ecb88
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-08-16 21:23:19 -0300

    reorganize projects list main page

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", () => {
+  const projects = [
+    { name: "article-preview-component" },
+    { name: "base-apparel-coming-soon" },
+    { name: "four-card-feature-section" },
+    { name: "fylo-data-storage-component" },
+    { name: "single-price-grid-component" },
+  ];
+
+  const projectsList = document.querySelector(".projects-list");
+
+  projects.forEach(({ name }, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.innerHTML = `
+      <a href="/${name}/index.html">
+        <p>${index + 1}. ${formatProjectName(name)}</p>
+        <img src="/${name}/design/desktop-design.jpg" alt="${name}" />
+      </a>     
+    `;
+
+    projectsList.appendChild(listItem);
+  });
+
+  function formatProjectName(name) {
+    return name
+      .split("-")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+});
